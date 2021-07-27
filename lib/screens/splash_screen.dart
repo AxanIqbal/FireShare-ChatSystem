1d44f2b1f5fb718adbfdf51b9dca14621b97eb9b
ahsan <axan.iqbal123@gmail.com>
2021-07-27 21:21:39 +0500

    init 3

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                backgroundColor: Theme.of(context).accentColor,
+              ),
+              Text("Splash")
+            ],
+          ),
+        ));
+  }
+}
